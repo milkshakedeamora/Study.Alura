5a2464ab81a92a8dd3b2f07b0b299518215abf07
Paula Alves <90431867+milkshakedeamora@users.noreply.github.com>
2024-06-05 21:56:09 -0300

    03

--- Python (Orientação a Objetos)/modelos/restaurantes.py
+++ Python (Orientação a Objetos)/modelos/restaurantes.py
@@ -1,18 +1,28 @@
 class Restaurante:
     restaurantes = []
     def __init__(self,nome,categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.title()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     def __str__(self):
-        return f'{self.nome} {self.categoria}'
-    def listar_restaurante():
-        for restaurante in Restaurante.restaurantes:
-          estado = 'ativo' if restaurante.ativo else 'inativo'
-          print(f"{restaurante.nome} restaurante {restaurante.categoria}  {estado}")  
-restaurante_praca = Restaurante('Praça','Gourmet')
+        return f'{self._nome} {self._categoria}'
+    
+    @classmethod
+    def listar_restaurante(cls):
+        for restaurante in cls.restaurantes:
+          print(f"{restaurante._nome} restaurante {restaurante._categoria} Ativo: {restaurante.ativo}")  
+          
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
+    
+    def alterar_ativo(self):
+        self._ativo = not self._ativo
+restaurante_praca = Restaurante('praça','Gourmet')
 print(vars(restaurante_praca))
 restaurante_pizza = Restaurante ('Pizza Express','Italiano')
+restaurante_pizza.alterar_ativo()
 print(restaurante_pizza)
+
 Restaurante.listar_restaurante()
